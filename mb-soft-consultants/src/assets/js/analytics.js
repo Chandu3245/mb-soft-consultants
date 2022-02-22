9883a76249a8eedfafcb4585b2f67aaac39fd510
Chandu3245 <chandu3245@gmail.com>
2022-02-22 21:52:49 +0530

    Enabled Google Analytics

---
+++ mb-soft-consultants/src/assets/js/analytics.js
@@ -0,0 +1,9 @@
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-RX31XYL65C"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-RX31XYL65C');
+// </script>
--- mb-soft-consultants/src/environments/environment.prod.ts
+++ mb-soft-consultants/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  trackingId: "G-RX31XYL65C"
 };
